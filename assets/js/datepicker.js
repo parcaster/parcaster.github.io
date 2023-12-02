03b3ddedba61d18aa7a7a21e858812b97a402079
ritahaffter <145849631+ritahaffter@users.noreply.github.com>
2023-12-02 10:24:25 +0100

    Update datepicker.js

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -1,3 +1,3 @@
 var now = new Date();
 now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
-document.getElementById('dt').value = now.toISOString().slice(0,16);
+document.getElementById('datepicker').value = now.toISOString().slice(0,16);
