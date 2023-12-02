6c722d992ca0b8ab450d18f2669d747db6333b70
ritahaffter <145849631+ritahaffter@users.noreply.github.com>
2023-12-02 15:29:03 +0100

    Delete assets/js/metadata.js

--- assets/js/metadata.js
+++
@@ -1,9 +0,0 @@
-const apiUrlMetadata = 'https://parcaster-2ff51b8db57e.herokuapp.com/metadata';
-
-async function getMetadata(){
-    const response = await fetch(apiUrlMetadata);
-    const metadata = await response.json();
-    console.log(metadata);
-}
-
-export { getMetadata };
