d135d2da361993db5dadb3fc4fac651556c0be6e
ritahaffter <145849631+ritahaffter@users.noreply.github.com>
2023-12-02 13:57:37 +0100

    Update metadata.js

--- assets/js/metadata.js
+++ assets/js/metadata.js
@@ -1,7 +1,7 @@
 const apiUrlMetadata = 'https://parcaster-2ff51b8db57e.herokuapp.com/metadata';
 
 async function getMetadata(){
-    const response = await fetch(apiUrlMetadatal, {mode: "no-cors"});
+    const response = await fetch(apiUrlMetadata, {mode: "no-cors"});
     const metadata = await resopnse.json();
     console.log(metadata);
 }
