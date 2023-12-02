59612b8dc1547c4d8f1fea048eef59a5075e64a3
ritahaffter <145849631+ritahaffter@users.noreply.github.com>
2023-12-02 12:20:45 +0100

    Update metadata.js

--- assets/js/metadata.js
+++ assets/js/metadata.js
@@ -1,9 +1,16 @@
-var apiUrl = 'https://pokeapi.co/api/v2/pokemon/ditto/';
-fetch(apiUrl).then(response => {
-  return response.json();
-}).then(data => {
-  // Work with JSON data here
-  console.log(data);
-}).catch(err => {
-  // Do something for an error here
-});
+const apiUrl = 'https://api.example.com/data';
+
+fetch(apiUrl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+        return response.json();
+    })
+    .then(data => {
+        console.log('API response:', data);
+        // Verarbeite die API-Daten hier
+    })
+    .catch(error => {
+        console.error('Fetch error:', error);
+    });
