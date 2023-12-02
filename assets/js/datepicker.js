39f789e92b200d7fd04a616fd120ee6ef2c0a45d
ritahaffter <145849631+ritahaffter@users.noreply.github.com>
2023-12-02 10:55:56 +0100

    Update datepicker.js

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -2,5 +2,12 @@ var now = new Date();
 now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
 document.getElementById('datepicker').value = now.toISOString().slice(0,16);
 
-datepicker.Attributes["min"] = now.AddDays(-3).toISOString().slice(0,16);
-datepicker.Attributes["max"] = now.AddDays(7).toISOString().slice(0,16);
+document.addEventListener('DOMContentLoaded', function() {
+  var today = new Date();
+  var futureDate = new Date();
+  futureDate.setDate(today.getDate() + 7); // Hier wird das Höchstdatum auf 7 Tage in der Zukunft gesetzt
+
+  var datetimeInput = document.getElementById('datepicker');
+  datetimeInput.min = today.toISOString().slice(0, 16); // Das Mindestdatum ist das aktuelle Datum
+  datetimeInput.max = futureDate.toISOString().slice(0, 16); // Das Höchstdatum ist 7 Tage in der Zukunft
+});
