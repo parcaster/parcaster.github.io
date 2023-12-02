d3d3e492eca2b71f85670d8672f091b56ef73897
ritahaffter <145849631+ritahaffter@users.noreply.github.com>
2023-12-02 10:43:42 +0100

    Update datepicker.js

--- assets/js/datepicker.js
+++ assets/js/datepicker.js
@@ -2,5 +2,5 @@ var now = new Date();
 now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
 document.getElementById('datepicker').value = now.toISOString().slice(0,16);
 
-datepicker.Attributes["min"] = DateTime.Now.AddDays(-3).toISOString().slice(0,16);
-datepicker.Attributes["max"] = DateTime.Now.AddDays(7).toISOString().slice(0,16);
+datepicker.Attributes["min"] = now.AddDays(-3).toISOString().slice(0,16);
+datepicker.Attributes["max"] = now.AddDays(7).toISOString().slice(0,16);
